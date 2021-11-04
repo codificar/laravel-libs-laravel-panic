55885e897f488da39b44f50f00da918aaf9e6025
Ricardo Cavalcanti <ricardo.cavalcanti@codificar.com.br>
2021-11-04 16:36:53 -0300

    created admin settings resources

---
+++ src/Http/resources/PanicGettingAdminResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Codificar\Panic\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PanicGettingAdminResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'panic_admin_id' => $this->panic_admin_id,
+            'panic_admin_phone_number' => $this->panic_admin_phone_number,
+            'panic_admin_email' => $this->panic_admin_email,
+        ];
+    }
+}
---
+++ src/Http/resources/PanicSettingAdminResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Codificar\Panic\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PanicSettingAdminResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'success' => true,
+            'panic_admin_id' => $this->panic_admin_id,
+            'panic_admin_phone_number' => $this->panic_admin_phone_number,
+            'panic_admin_email' => $this->panic_admin_email,
+        ];
+    }
+}
