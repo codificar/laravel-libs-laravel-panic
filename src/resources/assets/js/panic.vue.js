d6fa777c38563668ccc42aa541204bafd492100b
oto.luz <oto.luz@codificar.com.br>
2021-12-10 18:34:06 -0300

    add function de trans

--- src/resources/assets/js/panic.vue.js
+++ src/resources/assets/js/panic.vue.js
@@ -12,6 +12,11 @@ Vue.use(VueSweetalert2);
 Vue.component('loading', Loading);
 Vue.component('pagination', pagination);
 
+//Allows localization using trans()
+Vue.prototype.trans = (key) => {
+    return _.get(window.lang, key, key);
+};
+
 //Main vue instance
 new Vue({
     el: '#VueJs',
