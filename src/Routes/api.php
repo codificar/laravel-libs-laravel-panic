d308894fb3f0a44b51a6f97b6de3472aedff9829
Raphael Cangucu <raphael.cangucu@codificar.com.br>
2022-02-26 16:29:39 -0300

    fix api

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -1,20 +1,18 @@
 <?php
 Route::group(['prefix' => '/lib/panic'], function () {
-    Route::post('/save', 'Codificar\Panic\Http\PanicController@storePanicRequest');
-    Route::post('/delete', 'Codificar\Panic\Http\PanicController@deletePanicRequest');
+    Route::post('/save', 'Codificar\Panic\Http\Controllers\PanicController@storePanicRequest');
+    Route::post('/delete', 'Codificar\Panic\Http\Controllers\PanicController@deletePanicRequest');
 
-    Route::get('settings/', 'Codificar\Panic\Http\PanicController@getPanicButtonSettings');
-    Route::post('settings/save', 'Codificar\Panic\Http\PanicController@savePanicButtonSettings');
+    Route::get('settings/', 'Codificar\Panic\Http\Controllers\PanicController@getPanicButtonSettings');
+    Route::post('settings/save', 'Codificar\Panic\Http\Controllers\PanicController@savePanicButtonSettings');
 
-    Route::get('settings/segup', 'Codificar\Panic\Http\PanicController@getPanicSegupSettings');
-    Route::post('settings/save/segup', 'Codificar\Panic\Http\PanicController@savePanicSegupSettings');
+    Route::get('settings/segup', 'Codificar\Panic\Http\Controllers\PanicController@getPanicSegupSettings');
+    Route::post('settings/save/segup', 'Codificar\Panic\Http\Controllers\PanicController@savePanicSegupSettings');
 
-    Route::post("settings/save/admin", 'Codificar\Panic\Http\PanicController@savePanicAdminSettings');
-    Route::get('settings/admin', 'Codificar\Panic\Http\PanicController@getPanicAdminSettings');
+    Route::post("settings/save/admin", 'Codificar\Panic\Http\Controllers\PanicController@savePanicAdminSettings');
+    Route::get('settings/admin', 'Codificar\Panic\Http\Controllers\PanicController@getPanicAdminSettings');
 
-    Route::get('/', 'PanicController@index');
-
-	Route::get('/admins', 'Codificar\Panic\Http\PanicController@getAdminsToSettingsPage');
+	Route::get('/admins', 'Codificar\Panic\Http\Controllers\PanicController@getAdminsToSettingsPage');
 
     //create route to save the admin data that is needed
     //create request validating the needed data 
