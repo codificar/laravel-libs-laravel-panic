7c2f217e77ed6f37a23366cf0e95125c8a774a21
Luiz Antunes <luiz.antunes@codificar.com.br>
2022-07-04 14:35:29 -0300

    fix translations pt-ao

---
+++ src/resources/lang/ao/panic.php
@@ -0,0 +1,65 @@
+<?php
+
+return array(
+    'user' => 'O passageiro ',
+    'provider' => 'O motorista ',
+    'id' => ' com id ',
+    'emergency_alert' => ' gerou um Alerta de Emergência durante a corrida :request_id com o motorista ',
+    'emergency_alert_provider' => ' gerou um Alerta de Emergência durante a corrida :request_id com o passageiro ',
+    'email_text' => 'Verifique o painel de email assim que possível',
+    'document' => ' e documento ',
+    'vehicle' => ' no veículo ',
+    'no_security_agency' => 'o admin não tem um provedor de segurança público configurado',
+    'admin_phone_saved' => 'O telefone do Admin foi salvo na DB',
+    'admin_phone_not_saved' => 'O telefone do Admin não foi salvo na DB',
+    'successful_delete' => 'O registro foi apagado com sucesso',
+    'panic_sms_message' => 'Alerta de Emergência foi disparado, favor atentar a sua caixa de entrada do email registrado',
+    'panic_push_title' => 'Alerta de Pânico',
+    'panic_email_subject' => 'Alerta de Pânico',
+    'request_ledger_error' => 'ledger_id is required and must be an integer',
+    'request_request_id_error' => 'request_id is required and must be an integer',
+    'push_request_error' => 'A requisição de Push disparou uma exceção, favor verificar os parâmetros enviados',
+    'push_was_successful' => 'A requisição de push foi bem sucedida',
+    'save_segup_setting_was_not_successful' => 'A requisição para salvar os settings da SEGUP não foi bem sucedida',
+    'no_segup_login' => 'Não há login da SEGUP registrado no banco de dados',
+    'no_segup_password' => 'Não há senha da SEGUP registrada no banco de dados',
+    'no_segup_url' => 'Não há url da SEGUP registrada no banco de dados',
+    'no_segup_verification_url' => 'Não há url de verificação de token da SEGUP registrada no banco de dados',
+    'push_no_ledger' => 'Contato não encontrado para envio de notificação',
+	'panic_button' => 'Botão de pânico',
+
+	//palavras chaves
+	'yes' => 'Sim',
+	'no' => 'Não',
+	'save' => 'Salvar',
+
+	//settings
+	'panic_config' => 'Configurações do botão de pânico',
+
+	'panic_general_config' => 'Configurações gerais',
+	'panic_button_enabled_user' => 'Habilitar para o usuário?',
+	'panic_button_enabled_driver' => 'Habilitar para o motorista?',
+
+	//configurações segup
+	'panic_segup_config' => 'Configurações SEGUP',
+	'segup_login' => 'Login SEGUP',
+	'segup_password' => 'Senha SEGUP',
+	'segup_request_url' => 'URL de requisição SEGUP',
+	'segup_verification_url' => 'URL de verificação SEGUP',
+
+	//Configurações do administrador
+	'panic_admin_config' => 'Configurações do Administrador',
+	'admin' => 'Selecione o administrador',
+	'admin_phone' => 'Telefone do administrador',
+
+	//Relatório pânico
+	'filters' => 'Filtros',
+	'filter' => 'Filtrar',
+	'request_id' => 'Id da corrida',
+	'panic_user' => 'Usuário',
+	'panic_provider' => 'Motorista',
+	'panic_id' => 'Id',
+	'history' => 'Mensagem',
+	'data' => 'Data/Horário',
+
+);
