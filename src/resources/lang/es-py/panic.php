288387451fe273a0805c5d4d2ac676595010daa6
Randler Ferraz Almeida <randler.ferraz@codificar.com.br>
2023-09-22 16:27:59 -0300

    adicionado tradução es-py

---
+++ src/resources/lang/es-py/panic.php
@@ -0,0 +1,68 @@
+<?php
+
+return array(
+    'user' => 'El pasajero ',
+    'provider' => 'El conductor ',
+    'id' => ' con ID ',
+    'emergency_alert' => ' generó una Alerta de Emergencia durante el viaje :request_id con el conductor ',
+    'emergency_alert_provider' => ' generó una Alerta de Emergencia durante el viaje :request_id con el pasajero ',
+    'email_text' => 'Por favor, revise su correo electrónico tan pronto como sea posible',
+    'document' => ' y documento ',
+    'vehicle' => ' en el vehículo ',
+    'no_security_agency' => 'el administrador no tiene un proveedor de seguridad pública configurado',
+    'admin_phone_saved' => 'El teléfono del administrador se ha guardado en la base de datos',
+    'admin_phone_not_saved' => 'El teléfono del administrador no se ha guardado en la base de datos',
+    'successful_delete' => 'El registro se ha eliminado correctamente',
+    'panic_sms_message' => 'Se ha activado una Alerta de Emergencia, por favor, revise su bandeja de entrada de correo electrónico registrado',
+    'panic_push_title' => 'Alerta de Pánico',
+    'panic_email_subject' => 'Alerta de Pánico',
+    'request_ledger_error' => 'Se requiere ledger_id y debe ser un número entero',
+    'request_request_id_error' => 'Se requiere request_id y debe ser un número entero',
+    'push_request_error' => 'La solicitud de envío de notificación push generó una excepción, por favor, verifique los parámetros enviados',
+    'push_was_successful' => 'La solicitud de envío de notificación push se realizó con éxito',
+    'save_segup_setting_was_not_successful' => 'La solicitud para guardar la configuración de SEGUP no se realizó con éxito',
+    'no_segup_login' => 'No se ha registrado el inicio de sesión de SEGUP en la base de datos',
+    'no_segup_password' => 'No se ha registrado la contraseña de SEGUP en la base de datos',
+    'no_segup_url' => 'No se ha registrado la URL de solicitud de SEGUP en la base de datos',
+    'no_segup_verification_url' => 'No se ha registrado la URL de verificación de token de SEGUP en la base de datos',
+    'push_no_ledger' => 'No se encontró un contacto para enviar la notificación',
+    'panic_button' => 'Botón de Pánico',
+
+    // Palabras clave
+    'yes' => 'Sí',
+    'no' => 'No',
+    'save' => 'Guardar',
+
+    // Configuraciones
+    'panic_config' => 'Configuración del Botón de Pánico',
+
+    'panic_general_config' => 'Configuraciones Generales',
+    'panic_button_enabled_user' => '¿Habilitar para el usuario?',
+    'panic_button_enabled_driver' => '¿Habilitar para el conductor?',
+
+    // Configuraciones SEGUP
+    'panic_segup_config' => 'Configuraciones SEGUP',
+    'segup_login' => 'Inicio de Sesión SEGUP',
+    'segup_password' => 'Contraseña SEGUP',
+    'segup_request_url' => 'URL de Solicitud SEGUP',
+    'segup_verification_url' => 'URL de Verificación SEGUP',
+
+    // Configuraciones del Administrador
+    'panic_admin_config' => 'Configuraciones del Administrador',
+    'admin' => 'Seleccionar al Administrador',
+    'admin_phone' => 'Teléfono del Administrador',
+
+    // Reporte de Pánico
+    'filters' => 'Filtros',
+    'filter' => 'Filtrar',
+    'request_id' => 'ID del Viaje',
+    'panic_user' => 'Usuario',
+    'panic_provider' => 'Conductor',
+    'panic_id' => 'ID',
+    'history' => 'Mensaje',
+    'data' => 'Fecha/Hora',
+
+    "empty_chat" => "Chat vacío",
+    "show_all_panic_messages" => "Mostrar todos los mensajes de pánico",
+    "total" => "total"
+);
